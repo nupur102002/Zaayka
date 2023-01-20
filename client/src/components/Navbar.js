54920aa5665d9efce07d27a59ce3d939760de0e7
Nupur Goyal <nupur102002@gmail.com>
2023-01-20 20:51:57 +0530

    Context Created

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ const NavBar=()=>{
     return (
                <nav>
     <div className="nav-wrapper white">
-      <Link to="/" className="brand-logo">ZAGUAR</Link>
+      <Link to="/" className="brand-logo">ZAAYKA</Link>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/signup">Signup</Link></li>

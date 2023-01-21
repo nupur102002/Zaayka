d881b76ffc2b8eeb5214110b51434ea4e2279fe9
Nupur Goyal <nupur102002@gmail.com>
2023-01-21 13:47:25 +0530

    fetch recipes on profile

--- client/src/components/screens/Profile.js
+++ client/src/components/screens/Profile.js
@@ -1,46 +1,63 @@
-import React  from "react";
+import React, { useEffect, useState, useContext } from "react";
+import { UserContext } from "../../App";
 
-const Profile=()=>{
-    return (
-        <div style={{maxWidth:"550px",margin:"0px auto"}}>
+const Profile = () => {
+    const [mypics, setPics] = useState([])
+    const { state, dispatch } = useContext(UserContext)
 
-            <div  style={{  //for upper div(dp and name)
-               display:"flex",
-               justifyContent:"space-around",
-               margin:"20px 0px",
-              
-           }}>
- 
- {/* dp part */}
-            <div>  
-                <img style={{width:"160px",height:"160px",borderRadius:"80px", margin:"0px 0px"}} 
+    useEffect(() => {
+        fetch("/myrecipe", {
+            headers: {
+            "Authorization": "Bearer " + localStorage.getItem("jwt")
+        }
+        }).then(res => res.json())
+        .then(result => {
+            console.log(result);
+            setPics(result.myrecipe)
+        })
+}, [])
+return (
+    <div style={{ maxWidth: "550px", margin: "0px auto" }}>
+
+        <div style={{  //for upper div(dp and name)
+            display: "flex",
+            justifyContent: "space-around",
+            margin: "20px 0px",
+
+        }}>
+
+            {/* dp part */}
+            <div>
+                <img style={{ width: "160px", height: "160px", borderRadius: "80px", margin: "0px 0px" }}
                     src="https://images.newindianexpress.com/uploads/user/imagelibrary/2022/8/21/w600X390/Hrithik_Roshan_PTI.jpg"
                 />
 
                 {/* name part */}
-                </div>  
-                <div> 
-                <h4>Hrithik Roshan</h4>
-                  <div style={{display:"flex",justifyContent:"space-between",width:"108%"}}> 
-                     <h5>100 post </h5>
-                     <h5>100 follower </h5>
-                     <h5>100 following</h5>
-                  </div>
+            </div>
+            <div>
+                <h4>{state ? state.name : "loading"}</h4>
+                <h5>{state ? state.email : "loading"}</h5>
+                <div style={{ display: "flex", justifyContent: "space-between", width: "108%" }}>
+                    <h5>100 post </h5>
+                    <h5>100 follower </h5>
+                    <h5>100 following</h5>
                 </div>
             </div>
-   
-
-   {/* for pic uploaded part */}
-   <div className="gallery">
-           <img className="item" src="https://static.abplive.com/wp-content/uploads/2019/10/05084908/Hrithik-Roshan-site.jpg" />
-           <img className="item" src="https://static.abplive.com/wp-content/uploads/2019/10/05084908/Hrithik-Roshan-site.jpg" />
-           <img className="item" src="https://static.abplive.com/wp-content/uploads/2019/10/05084908/Hrithik-Roshan-site.jpg" />
-           <img className="item" src="https://static.abplive.com/wp-content/uploads/2019/10/05084908/Hrithik-Roshan-site.jpg" />
-   </div>
-
-
         </div>
-    )
+
+
+        {/* for pic uploaded part */}
+        <div className="gallery">
+            {
+                mypics.map(item => {
+                    return (
+                        <img key={item._id} className="item" src={item.photo} alt={item.title} />
+                    )
+                })
+            }
+        </div>
+    </div>
+)
 }
 
 export default Profile;

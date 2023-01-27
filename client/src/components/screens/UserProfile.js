94a37dd99bebd25e6bf0f8c83784e27d0cb2a0a1
harsh <harshsharma311003@gmail.com>
2023-01-27 11:19:06 +0530

    unfollow frontend part created & connectd to backend

--- client/src/components/screens/UserProfile.js
+++ client/src/components/screens/UserProfile.js
@@ -6,7 +6,7 @@ const Profile=()=>{
     const [userProfile,setProfile] = useState(null)
     const {state,dispatch} = useContext(UserContext)
     const {userid} = useParams()
-    const [showfollow,setShowFollow] = useState(true)
+    const [showfollow,setShowFollow] = useState(state?!state.following.includes(userid):true)
     console.log(userid)
     useEffect(()=>{
         fetch(`/user/${userid}`,{
@@ -47,6 +47,37 @@ const Profile=()=>{
              setShowFollow(false)
         })
     }
+    const unfollowUser = ()=>{
+        fetch('/unfollow',{
+            method:"put",
+            headers:{
+                "Content-Type":"application/json",
+                "Authorization":"Bearer "+localStorage.getItem('jwt')
+            },
+            body:JSON.stringify({
+                unfollowId:userid
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            
+            dispatch({type:"UPDATE",payload:{following:data.following,followers:data.followers}})
+             localStorage.setItem("user",JSON.stringify(data))
+            
+             setProfile((prevState)=>{
+                const newFollower = prevState.user.followers.filter(item=>item != data._id )
+                 return {
+                     ...prevState,
+                     user:{
+                         ...prevState.user,
+                         followers:newFollower
+                        }
+                 }
+             })
+             setShowFollow(true)
+             
+        })
+    }
+
     return (
         <>
         {userProfile? 
@@ -74,13 +105,25 @@ const Profile=()=>{
                                      <h6>{userProfile.user.followers.length} followers</h6>
                                      <h6>{userProfile.user.following.length} following</h6>
                                      </div>
-                                     <button style={{
+                {showfollow?
+                   <button style={{
                        margin:"10px"
                    }} className="btn waves-effect waves-light #64b5f6 blue darken-1"
                     onClick={()=>followUser()}
                     >
                         Follow
                     </button>
+                    : 
+                    <button
+                    style={{
+                        margin:"10px"
+                    }}
+                    className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                    onClick={()=>unfollowUser()}
+                    >
+                        UnFollow
+                    </button>
+                    }
                         </div>
          </div>
 

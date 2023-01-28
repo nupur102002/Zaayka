0d4fd7c389d47b62400f340ec39625606bf088cf
vijay kumar <vijaybhdgujjar@gmail.com>
2023-01-29 00:25:36 +0530

    Connecting update profile with backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -79,5 +79,13 @@ router.post('/search-users',(req,res)=>{
 
 })
 
-
+router.put('/updatepic',requireLogin,(req,res)=>{
+    User.findByIdAndUpdate(req.user._id,{$set:{pic:req.body.pic}},{new:true},
+        (err,result)=>{
+         if(err){
+             return res.status(422).json({error:"pic canot post"})
+         }
+         res.json(result)
+    })
+})
 module.exports=router;
